e664ac283369802df0ce46dbe3fb582d03c49afd
Aleksey <raven.lexa@yandex.ru>
2022-12-06 00:04:11 +0300

    FinalTaskTest

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+    длина которых меньше, либо равна 3 символам. 
+    Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+    При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+string[] ArraySmallestThreeCharacters(string[] arr)
+{
+    int j = 0;
+    string[] temparr = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3 && arr[i] != string.Empty)
+        {
+            temparr[j] = arr[i];
+            j++;
+        }
+    }
+
+    string[] resultarr = new string[j];
+
+    for (int i = 0; i < j; i++) resultarr[i] = temparr[i];
+    return resultarr;
+}
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    if (arr.Length > 1)
+    {
+        for (int i = 0; i < arr.Length - 1; i++) Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
+
+Console.WriteLine("Введите строки с разным количеством символов!");
+Console.WriteLine("Для разделения используйте пробел!");
+Console.WriteLine("Когда захотите остановиться, нажмите Enter");
+string[] result = ArraySmallestThreeCharacters(Console.ReadLine()!.Split(" "));
+
+Console.WriteLine("Массив из строк, длина которых меньше трех символов:");
+PrintArray(result);
+
